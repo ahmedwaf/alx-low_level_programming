dd5fa209b2feb9cba44820888077c754dd7571a5
Ahmed waleed <awf@example.com>
2023-09-08 12:31:20 -0700

    task2

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print the lower-case of alphabets
+ *
+ * Return: 0 (success)
+*/
+int main(void) {
+	char lowercase = 'a';
+	char uppercase = 'A';
+
+	while (lowercase <= 'z') {
+		putchar(lowercase);
+		lowercase++;
+		}
+
+	while (uppercase <= 'Z') {
+		putchar(uppercase);
+		uppercase++;
+		}
+
+	putchar('\n');
+
+	return (0);
+}
