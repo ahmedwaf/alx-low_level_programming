6dccb01b48646e5f4b8d7218b9716523b90b4633
Ahmed waleed <awf@example.com>
2023-09-12 19:41:22 -0700

    new task - new challange

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,12 +9,12 @@
  */
 int main(void)
 {
-	int sum = 0;
+	int sum;
 	int i;
 
 	for (i = 0 ; i < 1024 ; i++)
 	{
-		if (i % 3 == 0 || i % 5 == 0)
+		if ((i % 3 == 0 || i % 5 == 0))
 			sum += i;
 	}
 	}
