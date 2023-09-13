ea6938f9235473512f39845345a0bf965460b5c8
Ahmed waleed <awf@example.com>
2023-09-12 17:41:15 -0700

    new task - new challange

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,13 +21,6 @@ void times_table(void)
 	_putchar('0' + (result / 10));
 
 	_putchar('0' + (result % 10));
-
-	if (j < 9)
-	{
-	_putchar(',');
-	_putchar(' ');
-	}
-	else
 	{
 	_putchar('\n');
 	}
