590d84869b2621fb236467e26f1a6b7b49d58294
Ahmed waleed <awf@example.com>
2023-09-12 17:22:42 -0700

    new task - new challange

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,11 +9,11 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-	for (j = 0; j <= 9; j++)
-	{
-	result = i * j;
+		for (j = 0; j <= 9; j++)
+			{
+			result = i * j;
 
-	if (j == 0)
+			if (j == 0)
 	_putchar('0');
 	else if (result < 10)
 	_putchar(' ');
