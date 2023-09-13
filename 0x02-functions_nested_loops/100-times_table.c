2b8652ab6a3044585d7a2b0a8d6cff98b861d95c
Ahmed waleed <awf@example.com>
2023-09-12 19:16:53 -0700

    new task - new challange

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,34 +8,46 @@
  * Description: This function prints the times table up to n, following the
  * specified format.
  */
-void print_times_table(int n) {
-    if (n < 0 || n > 15) {
-        return;
+void print_times_table(int n)
+{
+	if (n < 0 || n > 15)
+{
+	return;
 	}
-
 	int row, column, product;
 
 	for (row = 0; row <= n; row++)
-	{
-	for (column = 0; column <= n; column++)
-	{
+{
+		for (column = 0; column <= n; column++)
+		{
 		product = row * column;
-	if (column == 0)
-	{
-		printf("%d", product);
-	}
-	else
-{
-		printf(", %2d", product);
-		}
-	}
-	printf("\n");
-	}
-}
 
-int main()
-{
-	int n = 12;
-	print_times_table(n);
-	return 0;
+		if (column != 0) {
+		_putchar(',');
+		_putchar(' ');
+
+		if (product < 10)
+		{
+		_putchar(' ');
+		}
+		}
+
+		if (product < 10)
+		{
+		_putchar(' ');
+		_putchar('0' + product);
+		{
+		else if (product >= 10 && product < 100) {
+		_putchar('0' + product / 10);
+		_putchar('0' + product % 10);
+		}
+		else
+		{
+		_putchar('0' + product / 100);
+		_putchar('0' + (product % 100) / 10);
+		_putchar('0' + product % 10);
+		}
+		}
+		_putchar('\n');
+	}
 }
