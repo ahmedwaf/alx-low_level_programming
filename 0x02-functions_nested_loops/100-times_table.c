e73e3a2480dd21de97e1e9b8a7c08a53ca9ffff7
Ahmed waleed <awf@example.com>
2023-09-13 12:59:04 -0700

    new task - new challange

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,52 +1,46 @@
 #include "main.h"
-#include <stdio.h>
+
 
 /**
- * print_times_table - Prints the n times table, starting with 0.
- * @n: The number of times tables to print (0 to 15).
- *
- * Description: This function prints the times table up to n, following the
- * specified format.
- */
+  * print_times_table - printsthe times inputed
+  * starting with 0.
+  *@n: the value of the timea table printed
+  */
+
+
 void print_times_table(int n)
 {
-	if (n < 0 || n > 15)
-{
-	return;
-	}
-	int row, column;
-	int product;
 
-	for (row = 0; row <= n; row++)
-{
-	for (column = 0; column <= n; column++)
-		
-		product = row * column;
 
-	if (col != 0)
-		_putchar(' ');
+	int num, mult, prod;
 
-	if (product < 10) {
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(product + '0');
-	}
-	else if (product < 100) {
-		_putchar(' ');
-		_putchar((product / 10) + '0');
-		_putchar((product % 10) + '0');
-	}
-	else
+
+	if (n >= 0 && n <= 15)
 	{
-		_putchar((product / 100) + '0');
-		_putchar(((product / 10) % 10) + '0');
-		_putchar((product % 10) + '0');
-		}
-
-	if (col < n)
-		_putchar(',');
-	else
-		_putchar('\n');
-		}
+	for (num = 0; num <= n; num++)
+	{
+	_putchar('0');
+	for (mult = 1; mult <= n; mult++)
+	{
+	_putchar(',');
+	_putchar(' ');
+	prod = num * mult;
+	if (prod <= 99)
+	_putchar(' ');
+	if (prod <= 9)
+	_putchar(' ');
+	if (prod >= 100)
+	{
+	_putchar((prod / 100) + '0');
+	_putchar(((prod / 10)) % 10 + '0');
+	}
+	else if (prod <= 99 && prod >= 10)
+	{
+	_putchar((prod / 10) + '0');
+	}
+	_putchar((prod % 10) + '0');
+	}
+	_putchar('\n');
+	}
 	}
 }
