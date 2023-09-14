6f4f00247729a450e72e17ef8296c3ef3d4057e2
Ahmed waleed <awf@example.com>
2023-09-14 09:40:42 -0700

    my debugging task

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Description: false or true  validation
+ *
+ * Return: 0 (success)
+ */
+
+void positive_or_negative(int i)
+{
+
+	if (i > 0)
+		printf("%i is positive\n", i);
+	else if (i == 0)
+		printf("%i is zero\n", i);
+	else
+		printf("%i is negative\n", i);
+}
