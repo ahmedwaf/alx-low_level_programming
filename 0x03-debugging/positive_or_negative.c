a9e35063c6b5fb3e136720d1a35b956aeeb738da
Ahmed waleed <awf@example.com>
2023-09-13 17:36:48 -0700

    my debugging task

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,6 +1,3 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -9,19 +6,13 @@
  * Description: false or true  validation
  *
  * Return: 0 (success)
-*/
-
-int main(void)
+ */
+void positive_or_negative(int i)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-		printf("%i is positive\n", n);
-	else if (n == 0)
-		printf("%i is zero\n", n);
+	if (i > 0)
+		printf("%i is positive\n", i);
+	else if (i == 0)
+		printf("%i is zero\n", i);
 	else
-		printf("%i is negative\n", n);
-	return (0);
+		printf("%i is negative\n", i);
 }
