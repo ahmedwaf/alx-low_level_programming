fa90f450693fce2fa277dfd10edd26ea194d5e2c
Ahmed waleed <awf@example.com>
2023-09-18 15:24:50 -0700

    5 task of C

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -14,16 +14,3 @@ void reset_to_98(int *n)
 {
 	*n = 98;
 }
-
-int main(void)
-{
-	int num = 42;
-
-	printf("Before reset_to_98: %d\n", num);
-
-	reset_to_98(&num);
-
-	printf("After reset_to_98: %d\n", num);
-
-	return (0);
-}
