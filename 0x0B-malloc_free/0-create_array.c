9880a5b998b7fbe8ff5499b5b665cf9152ca6210
Ahmed waleed <awf@example.com>
2023-10-04 14:24:05 -0700

    task 0x0B malloc and free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,12 +14,16 @@ char *create_array(unsigned int size, char c)
 	char *str;
 	unsigned int i;
 
-	if(size == 0)
+	if (size == 0)
+	{
 		return (NULL);
-	str = malloc(sizeof(char)*size);
-	if(size == NULL)
+	}
+	str = malloc(sizeof(char) * size);
+	if (size == NULL)
+	{
 		return (NULL);
-	for(i = 0;i < size;i++)
+	}
+	for (i = 0; i < size; i++)
 		str[i] = c
 			return (str);
 }
