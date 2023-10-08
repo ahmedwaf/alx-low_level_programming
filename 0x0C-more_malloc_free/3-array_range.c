aebf563756497faeb5c3709966ea3049571a1b0d
Ahmed waleed <awf@example.com>
2023-10-08 12:41:31 -0700

    second task of memory allocation

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,3 +1,14 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *array_range - creates an array of integers
+ * @min: minimum range of values stored
+ * @max: maximum range of values stored and number of elements
+ *
+ * Return: pointer to the new array
+ */
+
 int *array_range(int min, int max)
 {
 	char *ptr;
@@ -7,7 +18,7 @@ int *array_range(int min, int max)
 		return (NULL);
 	size = max - min + 1;
 
-	ptr = malloc(sizeof(*array_range) *size);
+	ptr = malloc(sizeof(*array_range) * size);
 
 	if (ptr == NULL)
 		return (NULL);
