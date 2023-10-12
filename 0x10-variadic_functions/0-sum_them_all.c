57deaf79863dd59011ae2fa9af92becacd930b35
Ahmed waleed <awf@example.com>
2023-10-12 11:03:53 -0700

    0x10 task

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,15 +11,11 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	if (n == 0)
-	{
-		return (0);
-	}
+	unsigned int i;
+	unsigned int sum = 0;
 	va_list args;
 
 	va_start(args, n);
-	int sum = 0;
-	int i;
 
 	for (i = 0; i < n; i++)
 		sum += va_arg(args, int);
