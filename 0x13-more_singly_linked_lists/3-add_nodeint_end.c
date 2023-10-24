7e69234095e4574220b69a28515ee3bb0c53fa65
ahmed_waleed <ahmed@example.com>
2023-10-24 15:46:46 -0700

    tasks

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -26,6 +26,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	while (current->next != NULL)
 		current = current->next;
 
-	current->next = new_node;
-	return (new_node);
+	current->next = new_mode;
+	return (new_mode);
 }
