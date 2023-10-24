a242273b5c84b764c0851d9e25b7a229d3630124
ahmed_waleed <ahmed@example.com>
2023-10-24 15:54:48 -0700

    tasks

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a list
+ * @head: pointer to start the list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current = *head;
+	listint_t *tempp;
+	while (current == NULL)
+	{
+		current = temp;
+		current = current->next;
+		free(tempp);
+	}
+	*head = NULL;
+}
