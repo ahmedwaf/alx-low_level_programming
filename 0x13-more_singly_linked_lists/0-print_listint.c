29babfec015758f20eb14c27a74bb26b0ce14378
ahmed_waleed <ahmed@example.com>
2023-10-24 13:40:35 -0700

    tasks

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * print_listint - prints the linked list
+ * @h: head of the list of elements
+ * Return: returns the count
+ */
+
+size_t print_listint(const listint_t *head)
+{
+	size_t count = 0;
+	const listint_t *current = head;
+
+	while (current != 0)
+	{
+		printf("%d\n", current->n);
+		count++;
+		current = current->next;
+	}
+	return (count);
+}
+n = print_listint(head);
+}
