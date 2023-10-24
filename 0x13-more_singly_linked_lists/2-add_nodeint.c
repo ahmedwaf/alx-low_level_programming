ad77b00a5724d59c9d94bf35408a658a2f96d08a
ahmed_waleed <ahmed@example.com>
2023-10-24 15:18:28 -0700

    tasks

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -7,7 +7,7 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new_mode = malloc(sizeof(listint));
+	listint_t *new_mode = malloc(sizeof(listin_t));
 
 	if (new_mode == NULL)
 	{
