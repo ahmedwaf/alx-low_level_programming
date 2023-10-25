83efcdce11880eb94b27b4afcdc3300e3297764a
ahmed_waleed <ahmed@example.com>
2023-10-24 17:48:27 -0700

    tasks

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		new->next = temp;
 		*head = new;
-		return (*head);
+		return (new);
 	}
 
 	while (len < idx - 1)
