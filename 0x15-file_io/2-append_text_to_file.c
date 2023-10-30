f2817bb3312a8cc045c97d218abad19e882fc690
ahmed_waleed <ahmed@example.com>
2023-10-30 16:05:27 -0700

    task3

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,29 +11,30 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file_descriptor;
-	ssize_t bytes_written;
+	int fd;
+	int nletters;
+	int rwr;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	if (text_content == NULL)
-		return (1);
+	fd = open(filename, O_WRONLY | O_APPEND);
 
-	file_descriptor = open(filename, O_WRONLY | O_APPEND);
-
-	if (file_descriptor == -1)
+	if (fd == -1)
 		return (-1);
 
-	bytes_written = write(file_descriptor, text_content, strlen(text_content));
-
-	if (bytes_written == -1)
+	if (text_content)
 	{
-		close(file_descriptor);
-		return (-1);
+		for (nletters = 0; text_content[nletters]; nletters++)
+			;
+
+		rwr = write(fd, text_content, nletters);
+
+		if (rwr == -1)
+			return (-1);
 	}
 
-	close(file_descriptor);
+	close(fd);
 
 	return (1);
 }
