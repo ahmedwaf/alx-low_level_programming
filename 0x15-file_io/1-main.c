9ef0b1a01a22ce5a1f22271b3cda45f8f8fb31a5
ahmed_waleed <ahmed@example.com>
2023-10-30 15:37:20 -0700

    task2

--- 0x15-file_io/1-main.c
+++ 0x15-file_io/1-main.c
@@ -1,37 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-#include <string.h>
+
 /**
  * main - check the code
  *
  * Return: Always 0.
- *
  */
-int create_file(const char *filename, char *text_content)
+int main(int ac, char **av)
 {
-	int bytes_written;
-	int fd;
+	int res;
 
-	if (filename == NULL)
+	if (ac != 3)
 	{
-		return (-1);
+		dprintf(2, "Usage: %s filename text\n", av[0]);
+		exit(1);
 	}
-	if (text_content == NULL)
-	{
-		text_content = "";
-	}
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (fd < 0)
-	{
-		return (-1);
-	}
-	bytes_written = write(fd, text_content, strlen(text_content));
-	if (bytes_written < 0) {
-		close(fd);
-		return (-1);
-	}
-	close(fd);
-
-	return (1);
+	res = create_file(av[1], av[2]);
+	printf("-> %i)\n", res);
+	return (0);
 }
