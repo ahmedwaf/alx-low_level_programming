24b6763938703a386e8e0932f1a2fb20feba4ac5
ahmed_waleed <ahmed@example.com>
2023-10-30 15:07:50 -0700

    task2

---
+++ 0x15-file_io/1-main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ *
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int bytes_written;
+	int fd;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	if (text_content == NULL)
+	{
+		text_content = "";
+	}
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd < 0)
+	{
+		return (-1);
+	}
+	bytes_written = write(fd, text_content, strlen(text_content));
+	if (bytes_written < 0) {
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+
+	return (1);
+}
